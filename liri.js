8777a12fce5c854b699206763859b0536840d378
Non Dewman <Dnewman1934@yahoo.com>
2019-05-03 15:14:17 -0500

    almost got that feeling lucky oo

--- liri.js
+++ liri.js
@@ -5,38 +5,27 @@ const fs = require("fs");
 const keys = require("./keys.js");
 const Spotify = require('node-spotify-api')
 const spotify = new Spotify(keys.spotify);
-const operator = process.argv[2];
+let operator = process.argv[2];
 let keyword = process.argv[3];
 exports.ticketmaster = {
     id: process.env.ticketmaster_ID,
     secret: process.env.ticketmaster
 }
-if (operator === 'search-concerts') {
-
-    axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${keyword}&apikey=${exports.ticketmaster.id}`).then(
-
-        function (response) {
-            const concertInformation = {
-                date: response.data._embedded.events[0].dates.start.localDate,
-                venue: response.data._embedded.events[0]._embedded.venues[0].name,
-                location: response.data._embedded.events[0].dates.timezone, //MM/DD/YYYY
-            };
-            console.log(concertInformation)
-        }).catch(function (error) {
-            if (error.response) {
-                console.log(error.response.data);
-            }
-            else if (error.request) {
-                console.log(error.request);
-            }
-            else {
-                console.log("Error", error.message);
-            }
-            console.log(error.config);
-        });
-};
-if (operator === 'search-movies') {
-
+function spotifySearch() {
+    spotify.search({ type: 'track', query: `${keyword}`, limit: 1, offset: 0 }, function (err, response) {
+        const songInfo = {
+            songName: response.tracks.items[0].name,
+            songPreview: response.tracks.items[0].preview_url,
+            songAlbum: response.tracks.items[0].album.name,
+            songArtist: response.tracks.items[0].album.artists[0].name,
+        }
+        console.log(songInfo)
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+    });
+}
+function searchMovies() {
     axios.get(`http://www.omdbapi.com/?t=${keyword}&apikey=trilogy`).then(
         function (response) {
             console.log(response.data.Title)
@@ -65,22 +54,66 @@ if (operator === 'search-movies') {
             console.log(error.config);
         });
 }
+function searchConcerts() {
+    axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${keyword}&apikey=${exports.ticketmaster.id}`).then(
 
-if (operator === 'search-songs') {
-    spotify.search({ type: 'track', query: `${keyword}`, limit: 1, offset: 0 }, function (err, response) {
-        const songInfo = {
-            songName: response.tracks.items[0].name,
-            songPreview: response.tracks.items[0].preview_url,
-            songAlbum: response.tracks.items[0].album.name,
-            songArtist: response.tracks.items[0].album.artists[0].name,
-        }
-        console.log(songInfo)
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-    });
+        function (response) {
+            const concertInformation = {
+                date: response.data._embedded.events[0].dates.start.localDate,
+                venue: response.data._embedded.events[0]._embedded.venues[0].name,
+                location: response.data._embedded.events[0].dates.timezone, //MM/DD/YYYY
+            };
+            console.log(concertInformation)
+        }).catch(function (error) {
+            if (error.response) {
+                console.log(error.response.data);
+            }
+            else if (error.request) {
+                console.log(error.request);
+            }
+            else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+};
+if (operator === 'search-concerts') {
+
+    searchConcerts();
 }
-// if (operator === 'feeling-lucky') { };
+if (operator === 'search-movies') {
+    searchMovies();
+}
+if (operator === 'search-songs') {
+    spotifySearch();
+}
+if (operator === 'feeling-lucky') {
+
+
+    fs.readFile("randomtext.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(data)
+        let datasplit = data.split();
+        console.log(datasplit);
+        operator = datasplit[0];
+        keyword = datasplit[1]
+        console.log(datasplit[0])
+        console.log(keyword)
+        console.log(operator)
+        if (operator === 'search-concerts') {
+            console.log('searchin concerts')
+            searchConcerts();
+        }
+        if (operator === 'search-movies') {
+            searchMovies();
+        }
+        if (operator === 'search-songs') {
+            spotifySearch();
+        }
+    })
+};
 
 
 
